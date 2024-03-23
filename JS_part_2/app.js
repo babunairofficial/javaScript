44c12d5267b5eb14aa9c9a443114b340888cc5ef
Babu Nair <babunairofficial@gmail.com>
2024-03-23 21:31:52 +0530

    Temperate Literals

--- JS_part_2/app.js
+++ JS_part_2/app.js
@@ -1,2 +1,18 @@
 console.log("Hello World");
 console.log("first javaScript");
+let a=10;
+let b=5;
+console.log("sum is ", a+b);
+//this is a comment
+
+
+let pencilPrice = 10;
+let eraserPrice = 5;
+console.log("The total price is : ", pencilPrice+eraserPrice, " Rupees.");
+//another way
+let penPrice = 20;
+let markerPrice = 30;
+// let output = "The total price is : " + (penPrice+markerPrice) + " Rupees.";
+//Temperate Literals
+let output = `The total price is : ${penPrice+markerPrice} Rupees`;
+console.log(output);
