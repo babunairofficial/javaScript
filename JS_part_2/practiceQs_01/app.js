6a5febcbbb037f97df5dafb6973ed4451e47a07b
Babu Nair <babunairofficial@gmail.com>
2024-03-24 00:51:48 +0530

    practice qs - if statement

---
+++ JS_part_2/practiceQs_01/app.js
@@ -0,0 +1,12 @@
+let color = "green";
+
+//Traffic Light System
+if(color == "red"){
+    console.log("Stop! The light is red.");
+}
+if(color == "yellow"){
+    console.log("Slow down! The light is yellow.");
+}
+if(color == "green"){
+    console.log("Go! The light is green.");
+}
