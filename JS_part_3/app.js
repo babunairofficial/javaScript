ab56d67095c93516bd753beb478a728d16cdad90
Babu Nair <babunairofficial@gmail.com>
2024-03-24 22:29:07 +0530

    String Methods - uppercase & lowercase

--- JS_part_3/app.js
+++ JS_part_3/app.js
@@ -1,5 +1,11 @@
 //String Methods
-//str.trim()
+//.trim()
 let msg = "           Hello          ";
 console.log(msg);
 console.log(msg.trim());
+
+//.toUpperCase() & .toLowerCase
+let name = "Babu Nair";
+let NAME = name.toUpperCase();
+console.log(NAME);
+console.log(name.toLowerCase());
