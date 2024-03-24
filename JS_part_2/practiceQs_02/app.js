57e79f717d7835493fc1b042ec973294873a200a
Babu Nair <babunairofficial@gmail.com>
2024-03-24 08:34:06 +0530

    practice qs - else if statement

---
+++ JS_part_2/practiceQs_02/app.js
@@ -0,0 +1,18 @@
+//Practice Qs
+//Create a system to calculate popcorn prices based on the size customer asked for
+let popcorn = "S";
+if(popcorn === "XL"){
+    console.log("Price is Rs. 250");
+}
+else if(popcorn === "L"){
+    console.log("Price is Rs. 200");
+}
+else if(popcorn === "M"){
+    console.log("Price is Rs. 100");
+}
+else if(popcorn === "S"){
+    console.log("Price is Rs. 50");
+}
+else{
+    console.log("Not available");
+}
