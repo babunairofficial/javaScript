47bf45ff61180089ce59d8b39cdd71c8916e90e9
Babu Nair <babunairofficial@gmail.com>
2024-03-24 16:25:18 +0530

    assignment qs Q3

--- JS_part_2/Assignment Questions/app.js
+++ JS_part_2/Assignment Questions/app.js
@@ -11,3 +11,20 @@ else{
 let name = prompt("enter your name");
 let age = prompt("enter your age");
 console.log(name,"is",age,"years old");
+
+//Q3
+ let quarter=4;
+ switch(quarter){
+    case 1:
+        console.log("Months in Quater 1 : January, February, March");
+        break;
+        case 2:
+        console.log("Months in Quater 2 : April, May, June");
+        break;
+        case 3:
+        console.log("Months in Quater 3 : July, August, September");
+        break;
+        case 4:
+        console.log("Months in Quater 4 : October, November, December");
+        break;
+ }
