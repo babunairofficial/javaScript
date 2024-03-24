048875a8b186ab2894e6662d891dbf22fb96f23b
Babu Nair <babunairofficial@gmail.com>
2024-03-24 09:25:13 +0530

    logical operator- practice qs

---
+++ JS_part_2/practiceQs_03/app.js
@@ -0,0 +1,9 @@
+// A "good string" is a string that starts with the letter 'a' & has a length > 3.
+// Write a program to find if a string is good or not. 
+let $tring = "apple";
+if($tring[0] === "a" && $tring.length > 3){
+    console.log("The string is good");
+}
+else{
+    console.log("Not a good string");
+}
