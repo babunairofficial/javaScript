f0aaf41c63e97afdf80c20a407d3e2b3015e3430
Babu Nair <babunairofficial@gmail.com>
2024-03-24 08:01:20 +0530

    else if statement

--- JS_part_2/app.js
+++ JS_part_2/app.js
@@ -49,3 +49,12 @@ if(age$ >= 18){
     console.log("you can vote");
 }
 console.log("after my if statement");
+
+//elseif statements
+let age1 = 14;
+if(age >= 18){
+    console.log("you can vote");
+}
+else if(age<18){
+    console.log("you cannot vote");
+}
