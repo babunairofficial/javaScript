81d9d9afe4014e0104c3f3111289c3bf07bc0b1c
Babu Nair <babunairofficial@gmail.com>
2024-03-25 09:07:47 +0530

    practice qs - array

---
+++ JS_part_3/practiceQs_02/app.js
@@ -0,0 +1,8 @@
+let months = ["january", "july", "march", "august"];
+console.log(months);
+months.shift();
+months.shift();
+console.log(months);
+months.unshift("june");
+months.unshift("july");
+console.log(months);
