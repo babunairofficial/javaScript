b4ad697f10284708f937199e9e7dc35e3bc9960c
Babu Nair <babunairofficial@gmail.com>
2024-03-25 07:39:50 +0530

    slice

--- JS_part_3/app.js
+++ JS_part_3/app.js
@@ -13,3 +13,9 @@ console.log(name.toLowerCase());
 //method chaining
 let newMsg = msg.trim().toUpperCase();
 console.log("new msg after method chaining: ",newMsg);
+
+//slice
+let prog = "JavaScript";
+console.log("slice from 0 to 4:",prog.slice(0,4));
+console.log("slice from 5th position to end of lenght:",prog.slice(5));
+console.log("slice from the end:",prog.slice(-1));
