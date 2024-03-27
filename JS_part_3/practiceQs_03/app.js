909565a7f7336ef56b05e925247a5fa025301469
Babu Nair <babunairofficial@gmail.com>
2024-03-27 22:27:43 +0530

    practice qs - array methods

--- JS_part_3/practiceQs_03/app.js
+++ JS_part_3/practiceQs_03/app.js
@@ -2,3 +2,7 @@ let months = ["january", "july", "march", "august"];
 console.log(months);
 months.splice(0,2, "july","june");
 console.log(months);
+
+//Qs 2
+let lang = ["c", "c++", "html", "javascript", "python", "java", "c#","sql"];
+console.log(lang.reverse().indexOf("javascript"));
