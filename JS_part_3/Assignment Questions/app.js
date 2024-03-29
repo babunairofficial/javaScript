52d3bacc6d340f762b74e1247a37cd5243278f3b
Babu Nair <babunairofficial@gmail.com>
2024-03-29 22:25:54 +0530

    assignment qs 03

--- JS_part_3/Assignment Questions/app.js
+++ JS_part_3/Assignment Questions/app.js
@@ -6,3 +6,12 @@ console.log(arr.splice(0,n));
 //Q2.
 let arr2 = [7,9,0,-2];
 console.log(arr2.splice(-n));
+
+//Q3
+let str = prompt("enter a string");
+if(str.length ==0) {
+console.log("the string is blank");
+}
+else {
+console.log("the string is not blank");
+}
