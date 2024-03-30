9adb7d8207f0307f90d231e8b19d65b8f93f08df
Babu Nair <babunairofficial@gmail.com>
2024-03-30 19:54:26 +0530

    print all odd numbers

--- JS_part_4/app.js
+++ JS_part_4/app.js
@@ -1,3 +1,10 @@
+//loops
 for(let i=1;i<=10;i++){
     console.log(i);
+}
+
+//print all odd numbers
+console.log("print odd numbers from 1 to 15");
+for(let j=1;j<=15;j=j+2){
+    console.log(j);
 }
