2214bd55ddc8395f62275ea099d5019be2e25bff
Babu Nair <babunairofficial@gmail.com>
2024-03-30 19:59:05 +0530

    print even numbers

--- JS_part_4/app.js
+++ JS_part_4/app.js
@@ -8,3 +8,9 @@ console.log("print odd numbers from 1 to 15");
 for(let j=1;j<=15;j=j+2){
     console.log(j);
 }
+
+//print even numbers
+console.log("print even numbers from 2 to 20");
+for(let k=2;k<=20;k=k+2){
+    console.log(k);
+}
