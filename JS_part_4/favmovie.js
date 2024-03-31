a26d247392a81b65f44c37a9314aaa834b70adde
Babu Nair <babunairofficial@gmail.com>
2024-03-31 22:26:39 +0530

    break keyword

--- JS_part_4/favmovie.js
+++ JS_part_4/favmovie.js
@@ -1,6 +1,11 @@
 let favmovie = "Avatar";
 let guess = prompt("guess favourite movie");
-while((guess!=favmovie)&&(guess!="quit")){
+while((guess!=favmovie)/*&&(guess!="quit")*/){
+    //break keyword
+    if(guess == "quit"){
+        console.log("you quit");
+        break;
+    }
     console.log("wrong guess");
     guess = prompt("wrong guess. try again");
 }
