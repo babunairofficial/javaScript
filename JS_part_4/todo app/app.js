efdaba1917a0f584b4bca20a95630978609ec4ad
Babu Nair <babunairofficial@gmail.com>
2024-04-01 12:52:59 +0530

    todo app - delete

--- JS_part_4/todo app/app.js
+++ JS_part_4/todo app/app.js
@@ -9,14 +9,17 @@ while(true){
     }
     if(req == "list"){
         console.log("---------------------");
-        for(task of todo){
-            console.log(task);
+        for(let i=0; i<todo.length; i++){
+            console.log(i, todo[i]);
         }
         console.log("---------------------");
     } else if(req == "add"){
         let task = prompt("please enter the task you want to add");
         todo.push(task);
         console.log("task added");
+    } else if(req == "delete"){
+        let idx = prompt("please enter the task index");
+        todo.splice(idx,1);
     }
     req = prompt("please enter your request");
 }
