f303465d97411677d95380f59c0ce3c4595a4339
Babu Nair <babunairofficial@gmail.com>
2024-04-02 22:31:45 +0530

    object literals

---
+++ JS_part_5/app.js
@@ -0,0 +1,15 @@
+//object literals
+let delhi = {
+    latitude: "28.7041 N",
+    longitude: "77.1025 E"
+};
+
+const student = {
+    name: "babu",
+    age: 32,
+    marks: 96.2,
+    city: "Delhi"
+};
+
+console.log(delhi);
+console.log(student);
