2101bc3150bf679f22e2b6673b39f157bf801d01
Babu Nair <babunairofficial@gmail.com>
2024-04-04 21:00:49 +0530

    javascript part 5 - Assignment Q2

--- JS_part_5/Assignment Questions/app.js
+++ JS_part_5/Assignment Questions/app.js
@@ -1,3 +1,11 @@
 //Q1
 let dice = Math.floor(Math.random()*6)+1;
 console.log("the number in dice roll is",dice);
+
+//Q2
+const car = {
+    name: prompt("enter name of car"),
+    model: prompt("enter model of car"),
+    color: prompt("enter color of car")
+};
+console.log("the car's name is ",car.name);
