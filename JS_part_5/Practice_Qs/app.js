4f40d7499a26719878923b7399ef2483acb8754b
Babu Nair <babunairofficial@gmail.com>
2024-04-04 19:58:42 +0530

    javascript part 5 - practice qs

---
+++ JS_part_5/Practice_Qs/app.js
@@ -0,0 +1,11 @@
+//Q1
+let random1 = Math.floor(Math.random()*100)+1;
+console.log("Random Number between 1 and 100 is",random1);
+
+//Q2
+let random2 = Math.floor(Math.random()*5)+1;
+console.log("Random Number between 1 and 5 is",random2);
+
+//Q3
+let random3 = Math.floor(Math.random()*5)+20;
+console.log("Random Number between 21 and 25 is",random3);
