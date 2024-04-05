d8f1c5bd150976353a12b539853f4bebfccf28ae
Babu Nair <babunairofficial@gmail.com>
2024-04-05 20:04:12 +0530

    functions with arguments

--- JS_part_6/app.js
+++ JS_part_6/app.js
@@ -5,3 +5,11 @@ function hello(){
 }
 /*function calling */
 hello();
+
+//Functions with arguments
+function printInfo(name,age){
+    console.log(`${name}'s age is ${33}.`);
+}
+
+printInfo("babu",33);
+printInfo(14);//here it is assigned as 14 belongs to 1st argument
