ce5009a45a86a3d2612c0134376df76ea164b70a
Babu Nair <babunairofficial@gmail.com>
2024-04-05 19:40:27 +0530

    javascript part 6 - practice Q1

---
+++ JS_part_6/Practice_Qs/app.js
@@ -0,0 +1,9 @@
+//Practice Q1
+function printPoem(){
+    console.log("Twinkle Twinkle, little star");
+    console.log("how I wonder what you are");
+    console.log("up above the world so high");
+    console.log("like a diamond in the sky");
+}
+
+printPoem();
