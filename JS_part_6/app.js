20f78b09c9f1c5baed0af436fa8f4c0bb3f4f122
Babu Nair <babunairofficial@gmail.com>
2024-04-05 20:37:59 +0530

    Return keyword

--- JS_part_6/app.js
+++ JS_part_6/app.js
@@ -13,3 +13,15 @@ function printInfo(name,age){
 
 printInfo("babu",33);
 printInfo(14);//here it is assigned as 14 belongs to 1st argument
+
+//Return keyword
+function sum(a,b){
+    console.log("hello");
+    return a+b;
+    console.log("will not print this line");
+}
+console.log(sum);
+let s = sum(3,4);
+console.log(s);
+console.log(sum(2,8));
+console.log(sum(sum(3,5),4));
