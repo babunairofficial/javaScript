8c2e37fd1da9e893dbaec3fe56ba0e4f0e417e59
Babu Nair <babunairofficial@gmail.com>
2024-04-07 19:08:45 +0530

    arrow functions

--- JS_part_7/app.js
+++ JS_part_7/app.js
@@ -26,3 +26,23 @@ try{
 console.log("hello2");
 console.log("hello2");
 console.log("hello2");
+
+//arrow functions
+const sum = (a,b) =>{
+    console.log(a+b);}
+sum(2,3);
+
+const cube = (n) =>{
+    return n*n*n;
+}
+console.log(cube(3));
+
+const pow = (a,b) =>{
+    return a ** b;
+}
+console.log(pow(2,3));
+
+const hello = () =>{
+    console.log("hello world");
+}
+hello();
