39c9249f59e6a6766767d288ce2c82c3cbb2b7cb
Babu Nair <babunairofficial@gmail.com>
2024-04-07 20:29:36 +0530

    javascript part 7 - assignment Q1

---
+++ JS_part_7/Assignment Questions/app.js
@@ -0,0 +1,10 @@
+//Q1
+let sum = 0;
+const arrayAverage = (arr) => {
+    for(let i = 0; i <arr.length;i++){
+        sum +=arr[i];
+    }
+    return sum/arr.length;
+}
+let arr = [1,2,3,4,5,6,7,8,9];
+console.log(arrayAverage(arr));
