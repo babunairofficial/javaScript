81e6e07943c0d368b5c5f343403176054b63e356
Babu Nair <babunairofficial@gmail.com>
2024-04-07 20:57:40 +0530

    javascript part 7 - assignment Q4

--- JS_part_7/Assignment Questions/app.js
+++ JS_part_7/Assignment Questions/app.js
@@ -22,10 +22,25 @@ isEven(987);
 //Q3
 const object = {
     message: "Hello, World!",
-    
+
     logMessage(){
         console.log(this.message);
     }
 };
 
 setTimeout(object.logMessage,1000);
+
+//Q4
+let length = 4;
+function callback(){
+    console.log(this.length);
+}
+
+const object2 = {
+    length: 5,
+    method(callback){
+        callback;
+    },
+};
+
+object2.method(callback, 1, 2);
