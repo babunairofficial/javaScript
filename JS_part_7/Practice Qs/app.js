0cbac05d43c7692370a0615d8b900d5aad6c4dda
Babu Nair <babunairofficial@gmail.com>
2024-04-07 20:15:32 +0530

    javascript part 7 - practice q2

--- JS_part_7/Practice Qs/app.js
+++ JS_part_7/Practice Qs/app.js
@@ -2,3 +2,12 @@ const square = (n) =>{
     return n*n;
 }
 console.log(square(5));
+
+//q2
+let id = setInterval(()=>{
+    console.log("Hello World");
+},2000);
+setTimeout(() => {
+    clearInterval(id);
+    console.log("clear interval run");
+}, 10000);
