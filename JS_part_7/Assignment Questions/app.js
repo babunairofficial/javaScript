7ff18c7b26ead0f7661677dac4e60debad6fe87e
Babu Nair <babunairofficial@gmail.com>
2024-04-07 20:35:04 +0530

    javascript part 7 - assignment Q2

--- JS_part_7/Assignment Questions/app.js
+++ JS_part_7/Assignment Questions/app.js
@@ -8,3 +8,13 @@ const arrayAverage = (arr) => {
 }
 let arr = [1,2,3,4,5,6,7,8,9];
 console.log(arrayAverage(arr));
+
+//Q2
+const isEven = (n) =>{
+    if(n%2==0){
+        console.log("even");
+    } else{
+        console.log("odd");
+    }
+}
+isEven(987);
