16cd10654ddfdec4ae1cde0a40787ec76afdc28f
Babu Nair <babunairofficial@gmail.com>
2024-04-08 20:02:38 +0530

    map

--- JS_part_8/app.js
+++ JS_part_8/app.js
@@ -31,3 +31,30 @@ console.log(arr1);
 arr1.forEach((player) => {
     console.log(player.score);
 });
+
+//map 
+let num = [1, 2, 3, 4];
+
+let double = num.map((el) => {
+    return el*2;
+});
+console.log(double);
+
+let students =[
+    {
+        name: "suresh",
+        marks: 95
+    },
+    {
+        name: "ramesh",
+        marks: 94.4
+    },
+    {
+        name: "rajesh",
+        marks: 92
+    }
+];
+let gpa = students.map((el) => {
+    return el.marks /10;
+});
+console.log(gpa);
