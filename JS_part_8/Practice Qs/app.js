c248c02577d5f26c394fd3df85f6bf94fd4cc19c
Babu Nair <babunairofficial@gmail.com>
2024-04-08 21:00:32 +0530

    javascript part 8 - practice q2

--- JS_part_8/Practice Qs/app.js
+++ JS_part_8/Practice Qs/app.js
@@ -3,3 +3,15 @@ let arr = [10, 20, 30, 40, 50];
 
 let result = arr.every((el)=>el%10==0);
 console.log(result);
+
+//q2
+let arr2 = [12,78,34,98,25,77];
+
+let min = arr2.reduce((n,el)=>{
+    if(n<el){
+        return n;
+    }else{
+        return el;
+    }
+});
+console.log(min);
