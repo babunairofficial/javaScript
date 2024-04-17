646ed4d3c77525c0457402abaedbbf86eea609fa
Babu Nair <babunairofficial@gmail.com>
2024-04-17 23:19:14 +0530

    DOM Events

---
+++ JS_part_10/app.js
@@ -0,0 +1,21 @@
+//onclick
+// let btn = document.querySelector("button");
+// console.dir(btn);
+// console.log(btn.onclick);
+// btn.onclick = function(){
+    // console.log("button was clicked");
+    // alert("button was clicked");
+// };
+function sayHello(){
+    alert("hello");
+};
+// btn.onclick = sayHello;
+
+//onmouseenter
+let btns = document.querySelectorAll("button");
+for(btn of btns){
+    btn.onclick = sayHello;
+    btn.onmouseenter = function(){
+        console.log("you entered a button");
+    };
+}
