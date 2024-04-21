c5fa247598ff9a47dc2075c37803f1d64034a46b
Babu Nair <babunairofficial@gmail.com>
2024-04-21 20:32:27 +0530

    event bubbling

---
+++ JS_mini_project/app.js
@@ -0,0 +1,20 @@
+//event bubbling
+let div = document.querySelector("div");
+let ul = document.querySelector("ul");
+let lis = document.querySelectorAll("li");
+
+div.addEventListener("click", function(){
+    console.log("div was clicked");
+});
+
+ul.addEventListener("click", function(event){
+    event.stopPropagation(); //to prevent event bubbling
+    console.log("ul was clicked");
+});
+
+for (li of lis){
+    li.addEventListener("click", function(event){
+        event.stopPropagation(); //to prevent event bubbling
+        console.log("li was clicked");
+    });
+}
