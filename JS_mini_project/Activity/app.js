890f998df39d1ab86d5da94dd640ef11a6619ba3
Babu Nair <babunairofficial@gmail.com>
2024-04-21 23:36:24 +0530

    TO Do app - add delete buttons with each task

--- JS_mini_project/Activity/app.js
+++ JS_mini_project/Activity/app.js
@@ -1,12 +1,26 @@
+//create an unordered list
 let ul = document.createElement("ul");
 let btn = document.querySelector("button");
 let input = document.querySelector("input");
 let div = document.querySelector("div");
+
+//append the unordered list to div element
 div.appendChild(ul);
+
+//function to perform on clicking the button
 btn.addEventListener("click", function(){
+    //add tasks as lists
     let lis = document.createElement("li");
     lis.innerText = input.value;
-    ul.append(lis);
+
+    //create delete buttons
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("delete");
+
+    //append delete button and lists to the unordered list
+    lis.appendChild(delBtn);
+    ul.appendChild(lis);
     input.value = "";
 });
 
