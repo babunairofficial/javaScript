31e2854cc5e006042e2630953b6db5af0942c01e
Babu Nair <babunairofficial@gmail.com>
2024-04-24 23:57:38 +0530

    callback hell

--- JS_part_11/app.js
+++ JS_part_11/app.js
@@ -48,3 +48,56 @@ setTimeout(() => {
     console.log("Hello world") 
  }, 2000); //this line will execute after 2 seconds
 console.log("hello....");
+
+
+//Callback Hell
+/*callback nesting => callback hell*/
+let h1 = document.querySelector("h1");
+
+function changeColor(color, delay,nextColorChange){
+    setTimeout(() => {
+        h1.style.color = color;
+        if(nextColorChange) nextColorChange();
+    }, delay);
+}
+
+changeColor("red",1000, () => {
+    changeColor("orange",1000, () => {
+        changeColor("green", 1000, () => {
+            changeColor("yellow", 1000, ()=> {
+                changeColor("pink", 1000);
+            });
+        });
+    });
+});
+
+
+/*
+let h1 = document.querySelector("h1");
+
+function changeColor(color, delay){
+    setTimeout(() => {
+        h1.style.color = color;
+    }, delay);
+}
+
+changeColor("red",1000);
+
+changeColor("orange",2000);
+
+changeColor("green",3000);
+*/
+
+/*
+setTimeout(changeColor("red") => {
+    h1.style.color = "red";
+}, 1000);
+
+setTimeout(() => {
+    h1.style.color = "orange";
+}, 2000);
+
+setTimeout(() => {
+    h1.style.color = "green";
+}, 3000);
+*/
