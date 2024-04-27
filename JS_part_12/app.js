ab19531b18e1985c5f1ab78db4e734c778a0979e
Babu Nair <babunairofficial@gmail.com>
2024-04-27 23:33:55 +0530

    await keyword

--- JS_part_12/app.js
+++ JS_part_12/app.js
@@ -15,3 +15,46 @@ greet()
 let demo = async () => {
     return 5;
 };
+
+
+//await keyword
+function getNum(){
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            let num = Math.floor(Math.random()*10)+1;
+            console.log(num);
+            resolve();
+        }, 1000);
+       
+    })
+}
+
+async function demo2(){
+    await getNum();
+    //to print with a delay, await keyword is used
+    await getNum();
+    getNum();
+}
+
+console.log(demo2());
+
+//color change code
+let h1 = document.querySelector("h1");
+
+function changeColor(color, delay){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            console.log(`color changed tp ${color}!`);
+            resolve("color changed!");
+        }, delay);        
+    });
+}
+
+async function demo3(){
+    await changeColor("red", 1000);
+    await changeColor("orange", 1000);
+    await changeColor("green", 1000);
+    changeColor("blue", 1000);
+}
+console.log(demo3());
