a3543d4a9c525680af892bf0e5ecd5364065d390
Babu Nair <babunairofficial@gmail.com>
2024-05-01 22:28:24 +0530

    activity

---
+++ JS_part_13/app2.js
@@ -0,0 +1,34 @@
+let url = "http://universities.hipolabs.com/search?name=";
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async() =>{
+    let country = document.querySelector("input").value;
+    console.log(country);
+    let colleges = await getColleges(country);
+    console.log(colleges);
+
+    show(colleges);
+})
+
+async function getColleges(country){
+    try{
+        let res = await axios.get(url + country);
+        return res.data;
+    } catch(e){
+        console.log(("error : ",e));
+    }
+}
+
+function show(colleges){
+    let list = document.querySelector("#list");
+    list.innerText = "";
+    for(col of colleges){
+        console.log(col.name);
+        
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li)
+    }    
+}
+
