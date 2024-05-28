4389f5fb9a582de416831d6b2e62b8e5b5ce8c0d
Babu Nair <babunairofficial@gmail.com>
2024-05-28 23:00:33 +0530

    variables and values

---
+++ from_youtube/app.js
@@ -0,0 +1,15 @@
+/*Datatypes*/
+
+var name = "babu";
+
+console.log(name);
+
+var a;
+var b = 5;
+
+console.log("a = ",a);
+console.log("b = ",b)
+
+a = 8;
+b = a;
+console.log(`new values : a = ${a} , b = ${b} `);
