5ef531a8b040fe25f76d0ecbad1f1dec75017a4b
Babu Nair <babunairofficial@gmail.com>
2024-05-28 23:20:12 +0530

    escaping literals

--- from_youtube/app.js
+++ from_youtube/app.js
@@ -13,3 +13,20 @@ console.log("b = ",b)
 a = 8;
 b = a;
 console.log(`new values : a = ${a} , b = ${b} `);
+
+//escaping literal quotes
+var myStr = " I am a \"double quoted\" string inside \"double quotes\"";
+console.log(myStr);
+/**
+ * Code Output
+ * \'   single quote
+ * \""  double quote
+ * \\   backslash
+ * \n   newline
+ * \r   carriage return
+ * \t   tab
+ * \b   backspace
+ * \f   form feed
+ */
+var mySTr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(mySTr); //variable names are case sensitive
