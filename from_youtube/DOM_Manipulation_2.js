6bca978c1aeffe5c90024e309014e44aa839fed5
Babu Nair <babunairofficial@gmail.com>
2024-06-17 12:21:11 +0530

    Event Listeners in javascript

---
+++ from_youtube/DOM_Manipulation_2.js
@@ -0,0 +1,11 @@
+//DOM Manipulation
+
+//Event Listeners
+
+//element.addEventListener("click", function);
+
+const button2 = document.querySelector('.btn-2');
+function alertBtn(){
+    alert('I also love JavaScript');
+};
+button2.addEventListener("click", alertBtn);
