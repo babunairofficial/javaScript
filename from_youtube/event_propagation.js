cb6962f2d3946de07ba9bf214718cd6924e5d3ae
Babu Nair <babunairofficial@gmail.com>
2024-06-25 22:41:25 +0530

    event propagation - capturing

--- from_youtube/event_propagation.js
+++ from_youtube/event_propagation.js
@@ -1,15 +1,34 @@
+//event Propagation - bubbling
+/*
 let div1 = document.querySelector("#div1"); 
-        let div2 = document.querySelector("#div2"); 
-        let div3 = document.querySelector("#div3"); 
+let div2 = document.querySelector("#div2"); 
+let div3 = document.querySelector("#div3"); 
   
-        div1.addEventListener("click", function (event) { 
-            alert("Component 1 event clicked"); 
-        }); 
+div1.addEventListener("click", function (event) {
+    alert("Component 1 event clicked");
+    });
+div2.addEventListener("click", function (event) { 
+    alert("Component 2 event clicked"); 
+    }); 
   
-        div2.addEventListener("click", function (event) { 
-            alert("Component 2 event clicked"); 
-        }); 
-  
-        div3.addEventListener("click", function (event) { 
-            alert("Component 3 event clicked"); 
-        }); 
+div3.addEventListener("click", function (event) { 
+    alert("Component 3 event clicked"); 
+    });
+*/
+
+//event propagation - capturing
+let div1 = document.querySelector("#div1"); 
+let div2 = document.querySelector("#div2"); 
+let div3 = document.querySelector("#div3"); 
+
+div1.addEventListener("click", function (event) { 
+    alert("Component 1 event clicked"); 
+    }, true); 
+
+div2.addEventListener("click", function (event) { 
+    alert("Component 2 event clicked"); 
+}, true); 
+
+div3.addEventListener("click", function (event) { 
+    alert("Component 3 event clicked"); 
+}, true);  
