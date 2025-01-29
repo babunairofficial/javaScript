ba12dff53a26639cf8c4b70605bfb2efe9d16636
Babu Nair <babunairofficial@gmail.com>
2025-01-29 12:20:51 +0530

    Javascript - alert

---
+++ doItYourself/app.js
@@ -0,0 +1,9 @@
+/*alerts*/
+
+alert("Welcome to Javascript"); 
+//alert is a keyword. 
+// within parenthesis we have a text string in double quotes (or even single quotes will do)
+//each line ends with a semicolon, except when curly brackets are used
+
+window.alert("alert using longer version");
+//alternatively window.alert can be used instead of the shorter version.
