0245fb798638df3128999c1bbb42e724a07a4247
Babu Nair <babunairofficial@gmail.com>
2025-02-02 10:25:30 +0530

    javascript - variable names legal and illegal

--- doItYourself/app.js
+++ doItYourself/app.js
@@ -41,3 +41,52 @@ alert(144);
 
 //alert can display a variable assigned to a number, but the value of the variable will be treated as a string.
 alert(i); //alert would display 12, as a string
+
+
+/*Variable Names Legal and Illegal*/
+
+//variable names cant be enclosed in quotes
+/* the following is a wrong usage:
+var "name"; 
+*/
+
+//variable names can never start with a number
+/* the following is a wrong usage:
+var 1stperson;
+*/
+
+//variable names cannot be a number
+/* the following is a wrong usage:
+var 1234;
+*/
+//variable names cannot have a space in between charaters
+/* the following is a wrong usage:
+var first name;
+*/
+
+//variable names can only contain letter, numbers, $, _
+var first_name = "aman";
+var second$name = "kumar";
+var rank1mark = 512;
+var $1total;
+var $total2;
+//console.log used to display the output in the terminal or in the console of the browser
+console.log(first_name);
+console.log(second$name);
+console.log(rank1mark);
+
+//variable names are case case-sensitive
+var fruit = "apple";
+var Fruit = "mango";
+console.log(fruit);
+console.log(Fruit);
+
+//prefer using camelCase convention for better readability
+var firstNameOfPerson = "Babu";
+console.log(firstNameOfPerson);
+
+// prefer using descriptive variable names to understand the purpose of the variable after prolonged absence or different user readability
+var firstNameOfPersonWhoWon = "mahendra";
+console.log(firstNameOfPersonWhoWon);
+
+//check terminal/console for outputs
