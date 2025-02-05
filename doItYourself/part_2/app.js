fad4d88f7b536a50b5b25778967d1c6746c91626
Babu Nair <babunairofficial@gmail.com>
2025-02-05 23:02:19 +0530

    javascript - arrays

---
+++ doItYourself/part_2/app.js
@@ -0,0 +1,16 @@
+/* Arrays */
+
+//arrays are similar to variables
+//arrays are used to assign multiple values with respect to each index
+//index number starts with 0
+
+var cities = ["Mumbai", "Delhi", "Kolkata", "Chennai"];
+
+// arrays can be assigned have mixed values in different indexes
+var mixedArray = [1, "India", 2, "Asia", 29, "Earth"];
+
+//naming of arrays follows the same rules of a variable
+//cities[0] = "Mumbai"
+//cities[3] = "Chennai"
+
+console.log("3rd element is having index 2, the value is " + cities[2]);
