fa26d67580507a15839e186e41444826b97572ca
Babu Nair <babunairofficial@gmail.com>
2025-02-06 22:43:25 +0530

    javascript - adding and removing elements

--- doItYourself/part_2/app.js
+++ doItYourself/part_2/app.js
@@ -14,3 +14,29 @@ var mixedArray = [1, "India", 2, "Asia", 29, "Earth"];
 //cities[3] = "Chennai"
 
 console.log("3rd element is having index 2, the value is " + cities[2]);
+
+
+/* Adding and removing elements in Arrays */
+
+//Adding elements
+//let us consider array named cities, which has been previously declared.
+cities[4] = "Pune";
+cities[5] = "Bengaluru";
+cities[7] = "Bhubaneswar";
+//value at any index can be null too
+//notice that the 7th element (index : 6) has been left out
+
+console.log(cities); //display the array in console
+
+//adding elements to the end using push keyword
+cities.push("Mysuru");
+cities.push("Madurai", "Thiruvananthapuram");
+
+console.log();
+console.log("array after pushing new elements to the end: " + cities);
+
+//removing elements
+cities.pop();
+//deletes the last element.
+
+console.log("array after deleting last element: " + cities);
