b4f995d09935dab8f924f8ea073855073b7f93a3
Babu Nair <babunairofficial@gmail.com>
2025-02-13 23:13:12 +0530

    javascript - functions

---
+++ doItYourself/part_3/app.js
@@ -0,0 +1,16 @@
+/* Functions */
+
+//functions are a block of code that needs to be executed multiple times, by invoking its name
+
+//create a function named 'multiply' to find the product of two numbers
+function multiply() { 
+    var a = prompt("enter the value of a");
+    var b = prompt ("enter the value of b");
+    var prod = a * b;
+    alert ("product of numbers is " + prod);    
+}
+
+//call the function 
+multiply();
+
+//note: naming of functions is similar to naming a variable, it follows the same properties.
